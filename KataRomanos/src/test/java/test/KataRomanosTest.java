7e504d52785a3347494e966ad01abbd0d8cd31c4
adgranados@gmail.com <e205@e205>
2016-08-19 20:55:31 -0500

    Se corrigen los tests dejando un assert por cada uno de ellos, se agregan los que se consideran necesarios.
    la idea es que solo se dejan varios asserts cuando la logica requiere la validacion de varias propiedades.

--- KataRomanos/src/test/java/test/KataRomanosTest.java
+++ KataRomanos/src/test/java/test/KataRomanosTest.java
@@ -34,39 +34,59 @@ public class KataRomanosTest {
     }
 
     @Test
-    public void validarConversionDeUnDigito() throws RomanosCeroException, RomanosNegativoException {
+    public void validarConversionDeUnDigitoII2() throws RomanosCeroException, RomanosNegativoException {
 
         String romano = ToRomanos.convertir(2);
         assertEquals(romano, "II");
         
-        romano = ToRomanos.convertir(3);
-        assertEquals(romano, "III");
-        
+
         romano = ToRomanos.convertir(7);
         assertEquals(romano, "VII");
         
         romano = ToRomanos.convertir(8);
         assertEquals(romano, "VIII");
     }
+    
+     @Test
+    public void validarConversionDeUnDigitoVII7() throws RomanosCeroException, RomanosNegativoException {
+
+        String romano = ToRomanos.convertir(7);
+        assertEquals(romano, "VII");
+    }
+    
 
     @Test
-    public void validarConversionDeDigitosEspeciales() throws RomanosCeroException, RomanosNegativoException {
+    public void validarConversionDeDigitosEspecialesIV4() throws RomanosCeroException, RomanosNegativoException {
         String romano = ToRomanos.convertir(4);
         assertEquals(romano, "IV");
         
-        romano = ToRomanos.convertir(9);
+    }
+    @Test
+    public void validarConversionDeDigitosEspecialesIX9() throws RomanosCeroException, RomanosNegativoException {
+        String romano = ToRomanos.convertir(9);
         assertEquals(romano, "IX");
         
-        romano = ToRomanos.convertir(40);
+    }
+    
+     @Test
+    public void validarConversionDeDigitosEspecialesXL40() throws RomanosCeroException, RomanosNegativoException {
+        String romano = ToRomanos.convertir(40);
         assertEquals(romano, "XL");
         
         romano = ToRomanos.convertir(90);
         assertEquals(romano, "XC");
         
     }
+    
+     @Test
+    public void validarConversionDeDigitosEspecialesXC90() throws RomanosCeroException, RomanosNegativoException {
+        String romano = ToRomanos.convertir(90);
+        assertEquals(romano, "XC");
+        
+    }
 
     @Test
-    public void validarConversionDeVariosDigitos() throws RomanosCeroException, RomanosNegativoException {
+    public void validarConversionDeVariosDigitosXX20() throws RomanosCeroException, RomanosNegativoException {
 
         String romano = ToRomanos.convertir(20);
         assertEquals(romano, "XX");
@@ -74,29 +94,74 @@ public class KataRomanosTest {
         assertEquals(romano, "XLIX");
 
     }
+    @Test
+    public void validarConversionDeVariosDigitosXLIX49() throws RomanosCeroException, RomanosNegativoException {
+
+        String romano = ToRomanos.convertir(49);
+        assertEquals(romano, "XLIX");
+
+    }
 
     @Test
-    public void validarConversionDeNumeralesBasicos() throws RomanosCeroException, RomanosNegativoException {
+    public void validarConversionDeNumeralBasicoI1() throws RomanosCeroException, RomanosNegativoException {
 
         String romano = ToRomanos.convertir(1);
         assertEquals(romano, "I");
-        romano = ToRomanos.convertir(5);
+
+    }
+    @Test
+    public void validarConversionDeNumeralBasicoV5() throws RomanosCeroException, RomanosNegativoException {
+
+        String romano = ToRomanos.convertir(5);
         assertEquals(romano, "V");
-        romano = ToRomanos.convertir(10);
+
+    }
+    @Test
+    public void validarConversionDeNumeralBasicoX10() throws RomanosCeroException, RomanosNegativoException {
+
+        String romano = ToRomanos.convertir(10);
         assertEquals(romano, "X");
-        romano = ToRomanos.convertir(50);
+
+    }
+    @Test
+    public void validarConversionDeNumeralBasicoL50() throws RomanosCeroException, RomanosNegativoException {
+
+        String romano = ToRomanos.convertir(50);
         assertEquals(romano, "L");
-        romano = ToRomanos.convertir(100);
+
+    }
+    @Test
+    public void validarConversionDeNumeralBasicoC100() throws RomanosCeroException, RomanosNegativoException {
+
+        String romano = ToRomanos.convertir(100);
         assertEquals(romano, "C");
-        romano = ToRomanos.convertir(500);
+
+    }
+    @Test
+    public void validarConversionDeNumeralBasicoD500() throws RomanosCeroException, RomanosNegativoException {
+
+        String romano = ToRomanos.convertir(500);
         assertEquals(romano, "D");
-        romano = ToRomanos.convertir(1000);
+
+    }
+    @Test
+    public void validarConversionDeNumeralBasicoM1000() throws RomanosCeroException, RomanosNegativoException {
+
+        String romano = ToRomanos.convertir(1000);
         assertEquals(romano, "M");
 
     }
     
+        
+        
+        
+        
+        
+        
+    
   /**
    * 
+     * @throws java.lang.Exception
    * @toDO  Codigo revisado por Daniel ALfonso  y Felipe Marciales
    */ 
 
